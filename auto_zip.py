9b013c71730e4c89fbfce63aa9853c49acee59ad
u_olimpiev <y.olimpiev@g.nsu.ru>
2020-11-16 11:26:26 +0700

    Version 0.9

--- auto_zip.py
+++ auto_zip.py
@@ -1,10 +1,17 @@
 import sys
 import os
+
 dir_name = 'selenium'
-filename_with_version = 'properties'
+filename_with_version = 'gradle.properties'
 
 if len(sys.argv) == 2 and sys.argv[1] == '--help':
-	print('Documentation')
+	print('Script for archiving a folder indicating the version of the package.\n') 
+	print('By default, the version is read from a file gradle.properties,\n')
+	print('a folder named selenium is archived \n')
+	print('(the file and folder must be in the same directory with the script).\n')
+	print('The script accepts two values as command line arguments - \n')
+	print('the name of the archive folder and the name of the version file.c\n')
+	print('example : python3.8 auto_zip.py name_folder file_with_version\n')
 
 if len(sys.argv) == 3:
 	dir_name = sys.argv[1]
